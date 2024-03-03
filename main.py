260faf0fdbf313c7aefa8b82bded411f8407a520
powellnatasa <31070521-powellnatasa@users.noreply.replit.com>
2024-03-03 21:37:13 +0000

    Added main-py

---
+++ main.py
@@ -0,0 +1,29 @@
+#creating a README.md file
+touch README.md
+
+#checking current working directory
+pwd
+
+#creating new folder 
+mkdir pythoncode
+
+#move to new folder using cd command
+cd pythoncode
+pwd
+
+#rename main.py 01_folder_structure.py
+mv main.py 01_folder_structure.py 
+
+#create new file using touch command
+touch 02_for_loop.py
+
+touch main.py
+mv main.py 02_for_loop.py
+
+touch main.py
+mv main.py 03_list_dictionary.py 
+
+
+#github
+git init
+git status
